e0fc566cea1f0f8d313f5da6119f8b6a2c11584d
meekoder <meekolydm@yahoo.com>
2020-09-04 14:23:45 -0700

    fixed function for image errors

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+const addHandleClickProp = () => {
+  streams.home.map(obj => {
+    obj.handleProfileClick = () => {
+      const username = obj.user;
+      const userTwiddles = streams.users[username];
+      $('#new-twiddles').remove();
+      $('#main').empty();
+      displayHomeFeed(userTwiddles);
+    }
+    return obj;
+  });
+};
+
+const handleLoadNewTwiddles = () => {
+  addHandleClickProp();
+  const newTwiddlesLength = streams.home.length - callCount; 
+  const newTwiddlesArr = streams.home.slice(newTwiddlesLength);
+  callCount = 0;
+  $('#new-twiddles').remove();
+  displayHomeFeed(newTwiddlesArr);
+};
+
 const formatDate = (dateObj) => {
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   let hours = dateObj.getHours();
@@ -19,59 +41,57 @@ const formatDate = (dateObj) => {
   return formattedDate;
 };
 
-const displayHomeFeed = (twiddleObj) => {
-  const username = twiddleObj.user;
-  const twiddle = twiddleObj.message;
-  const dateCreated = formatDate(twiddleObj.created_at);
-  const $main = $('#main');
-  const $article = $('<article class="media" id="#a-twiddle" />');
-  const $userMedia = $('<div class="media-content" />');
-  const $tweet = $('<div class="content" />');
-  const $user = $('<p />');
-  const $image = $('<img />');
-  const $profilePic = $('<figure class="media-left" />');
-  const $profileSize = $('<p class="image is-64x64" />');
-  const $date = $('<div class="level-right" />');
-  const $interaction = $(`<nav class='level is-mobile'>
+// const imgError = () => {
+//   $('#profile-icon').attr('src', './assets/default.png');
+// };
+
+const displayHomeFeed = (arrOfTwiddles) => {
+  addHandleClickProp();
+  arrOfTwiddles.forEach(twiddleObj => {
+    const username = twiddleObj.user;
+    const twiddle = twiddleObj.message;
+    const dateCreated = formatDate(twiddleObj.created_at);
+    const $main = $('#main');
+    const $article = $('<article class="media" />');
+    const $userMedia = $('<div class="media-content" />');
+    const $tweet = $('<div class="content" />');
+    const $user = $('<p />');
+    const $image = $('<img  id="profile-icon"/>');
+    const $profilePic = $('<figure class="media-left" />');
+    const $profileSize = $('<p class="image is-64x64" />');
+    const $date = $('<div class="level-right" />');
+    const $interaction = $(`<nav class='level is-mobile'>
           <div class='level-left'>
             <a class='level-item'>
               <span class='icon is-small'><i class='fas fa-heart'></i></span>
             </a>
           </div>
         </nav>`);
-  $date.text(`${dateCreated}`)
-    .appendTo($interaction);
-  $image.attr('src', `./assets/${username}.png`)
-    .css('cursor', 'pointer')
-    .appendTo($profileSize)
-    .click(twiddleObj.handleProfileClick);
-  $user.click(twiddleObj.handleProfileClick);
-  $profilePic.append($profileSize);
-  $tweet.append('<p />')
-    .text(`${twiddle}`)
-  $user.text(`@${username}`)
-    .css({'font-weight': 'bold',
-          'cursor': 'pointer'
+    $date.text(`${dateCreated}`)
+      .appendTo($interaction);
+    $image.attr({
+      'src': `./assets/${username}.png`,
+      'onerror': `this.src="./assets/default.png"`
     })
-    .prependTo($tweet);
-  $userMedia.append($tweet, $interaction);
-  $article.append($profilePic, $userMedia)
-    .prependTo($main);
-};
-
-const addHandleClickProp = () => {
-  streams.home.map(obj => {
-    obj.handleProfileClick = () => {
-      const username = obj.user;
-      const userTwiddles = streams.users[username];
-      $('#new-twiddles').remove();
-      $('#main').empty();
-      userTwiddles.forEach(x => displayHomeFeed(x));
-    }
-    return obj;
+      .css('cursor', 'pointer')
+      .appendTo($profileSize)
+      .click(twiddleObj.handleProfileClick);
+    $user.click(twiddleObj.handleProfileClick);
+    $profilePic.append($profileSize);
+    $tweet.append('<p />')
+      .text(`${twiddle}`)
+    $user.text(`@${username}`)
+      .css({'font-weight': 'bold',
+        'cursor': 'pointer'
+      })
+      .prependTo($tweet);
+    $userMedia.append($tweet, $interaction);
+    $article.append($profilePic, $userMedia)
+      .prependTo($main);
   });
 };
 
+// Counts number of times addTweet is called
 let callCount = 0;
 const originalAddTweet = addTweet;
 
@@ -80,6 +100,7 @@ addTweet = function(...arguments) {
   if (callCount === 1) {
     $('#new-twiddle-card').after('<button class="button is-info is-rounded" id="new-twiddles" />');
   }
+
   newTweetTrigger(...arguments);
   return originalAddTweet(...arguments);
 }
@@ -89,30 +110,30 @@ const newTweetTrigger = () => {
     .click(handleLoadNewTwiddles);
 };
 
-const handleLoadNewTwiddles = () => {
-  addHandleClickProp();
-  const newTwiddlesLength = streams.home.length - callCount; 
-  const newTwiddlesArr = streams.home.slice(newTwiddlesLength);
-  callCount = 0;
-  $('#new-twiddles').remove();
-  newTwiddlesArr.forEach(x => displayHomeFeed(x));
-};
-
 $(document).ready(function() {
   addHandleClickProp();
-  streams.home.forEach(x => displayHomeFeed(x));
+  console.log(streams)
+  displayHomeFeed(streams.home);
+
+  const handleNewTwiddle = () => {
+    const username = $('#username').val();
+    const twiddle = $('#twiddle').val();
+    const newTwiddle = {};
+    newTwiddle.user = username;
+    newTwiddle.message = twiddle;
+    newTwiddle.created_at = new Date();
+    if (!streams.users[username]) {
+      streams.users[username] = [];
+    }
+    addTweet(newTwiddle);
+    displayHomeFeed([newTwiddle]);
+  }
+
+  $('#add-new-twiddle').click(handleNewTwiddle);
 
   $('#twitter-icon, #home').click(function() {
-    addHandleClickProp();
     callCount = 0;
     $('#new-twiddles').remove();
-    streams.home.forEach(x => displayHomeFeed(x));
-    console.log('icon clicked')
-  })
+    displayHomeFeed(streams.home)
+  });
 });
-
-// const handleNewTwiddle = () => {
-
-// }
-
-// $('#twiddle').click(handleNewTwiddle);
